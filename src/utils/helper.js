59850d84e4cfaedbb54ddb471b1b736ecb2e1050
Warri <warriisaac@outlook.com>
2020-01-19 19:51:47 +0100

    Utilities (utils) for data validation is added

---
+++ src/utils/helper.js
@@ -0,0 +1,69 @@
+import jwt_decode from 'jwt-decode';
+import history from './history';
+
+/**
+ * @description decodes token
+ * 
+ * @param {string} token
+ * 
+ * @returns {void} void
+ */
+
+ const decodeToken = token => jwt_decode(token);
+
+ /**
+  * @description redirects users to appropriate page
+  * 
+  * @function reDirect
+  * 
+  * @param {string} token
+  * 
+  * @return {void} void
+  */
+
+const reDirect = (token) => {
+    const decodeToken = jwt_decode(token);
+    if (decodeToken.admin === 'true') {
+        history.push('/admin');
+    } else if (decodeToken.admin === false) {
+        history.push('/user');
+    }
+};
+
+/**
+ * @description log users out
+ * 
+ * @function logout
+ * 
+ * @param {void} void
+ * 
+ * @return {void} void
+ */
+
+const logout = () => {
+    window.localStorage.removeItem('token');
+    history.push('/');
+};
+
+/**
+ * @description pass the token to header
+ * 
+ * @function authorization
+ * 
+ * @param {void} void
+ * 
+ * @return {void} void
+ */
+
+const authorization = () => ({
+    headers: {
+        'x-access-token': localStorage.token
+    }
+});
+
+export {
+    decodeToken,
+    reDirect,
+    logout,
+    authorization
+}
---
+++ src/utils/officeValidation.js
@@ -0,0 +1,17 @@
+import Validatior from 'validatorjs';
+
+const rules = {
+    type: 'required|string|Federal|State|Local',
+    name: 'required|string'
+};
+
+const officeValidation = (data) => {
+    const validation = new Validator(data, rules);
+    const validationObj = { isValid: () => validation.passes() };
+    if (validation.fails()) {
+      validationObj.errors = validation.errors.all();
+    }
+    return Object.freeze(validationObj);
+};
+
+export default officeValidation;
